a52899eb15d0bcf8b2f9c2aa4ab5a16060d53c92
Sebastian Iulian Poiana <sebastianpoiana2015@gmail.com>
2020-03-19 19:36:29 +0000

    Events Description
    -I have developed another 4 functions named dragOver, dragEnter, DragLeave and DragDrop.
    -These functions create the events for the drag and drop process as a step-by-step series of happenings.
    -The console.log lines are used only for me to be able to see when each event happens by checking the console at the preview state, but now they will be taken out

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ const empties = document.querySelectorAll('.empty');
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
 
+// Loop through empties and call drag events
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
 // Drag Functions
 function dragStart() {
   this.className += ' hold';
@@ -12,5 +20,22 @@ function dragStart() {
 }
 
 function dragEnd() {
-  this.className ='fill';
+  this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  console.log('over');
+}
+
+function dragEnter() {
+  console.log('enter');
+}
+
+function dragLeave() {
+  console.log('leave');
+}
+
+function dragDrop() {
+  console.log('drop');
 }
